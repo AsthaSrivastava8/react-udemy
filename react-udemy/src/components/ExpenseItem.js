5b814fcd03eb3acc52fc56f84e2ae33efffdb00b
Astha <srivastavastha811@gmail.com>
2021-05-02 02:52:54 +0530

    added css

--- react-udemy/src/components/ExpenseItem.js
+++ react-udemy/src/components/ExpenseItem.js
@@ -1,5 +1,19 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-  return <h2>Expense Item!</h2>;
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseDescription = "Car insurance";
+  const expensePrice = 294.67;
+
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseDescription}</h2>
+        <div className="expense-item__price">${expensePrice}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
