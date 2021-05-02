208213cc4be0758d8679dab9a745ef64a6cfea2d
Astha <srivastavastha811@gmail.com>
2021-05-02 19:26:43 +0530

    refactored

--- react-udemy/src/components/Expenses/Expenses.js
+++ react-udemy/src/components/Expenses/Expenses.js
@@ -2,7 +2,7 @@ import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 
-function Expenses(props) {
+const Expenses = (props) => {
   return (
     <Card className="expenses">
       <ExpenseItem
@@ -27,7 +27,7 @@ function Expenses(props) {
       />
     </Card>
   );
-}
+};
 
 // or
 
